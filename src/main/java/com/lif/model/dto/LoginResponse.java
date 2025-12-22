8bf78e4a717c2b3d2ae1d5093743bc52046e1a86
Happid <happidilmi@gmail.com>
2025-12-22 20:25:45 +0700

    fix: error login response

--- src/main/java/com/lif/model/dto/LoginResponse.java
+++ src/main/java/com/lif/model/dto/LoginResponse.java
@@ -9,11 +9,4 @@ public class LoginResponse {
 
     private String token;
 
-    public LoginResponse(String token) {
-        this.token = token;
-    }
-
-    public String getToken() {
-        return token;
-    }
 }
