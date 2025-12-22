3ef452d2db1e40b46116b74f057bbf61fa88f03f
Happid <happidilmi@gmail.com>
2025-12-22 19:16:01 +0700

    Initial commit: Spring Boot Project

---
+++ src/test/java/com/lif/LifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
