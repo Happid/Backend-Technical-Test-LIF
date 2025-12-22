ec7c3b6c47c3eeb93fc454ff85fa97cfcc71226a
Happid <happidilmi@gmail.com>
2025-12-22 20:11:43 +0700

    feat: add dependency jwt, add jwt util and dto for login request and response

---
+++ src/main/java/com/lif/config/JwtUtil.java
@@ -0,0 +1,47 @@
+package com.lif.config;
+
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private static final long EXPIRATION_TIME = 86400000 * 7; // 7 hari
+    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(Long userId) {
+        return Jwts.builder()
+                .setSubject(String.valueOf(userId))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+
+    public Long extractUserId(String token) {
+        return Long.parseLong(
+                Jwts.parserBuilder()
+                        .setSigningKey(key)
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody()
+                        .getSubject()
+        );
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+}
